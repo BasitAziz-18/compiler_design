cec7f478c9f0d207aaa77634b89038b425491042
BASIT AZIZ <106722489+BasitAziz-18@users.noreply.github.com>
2025-03-25 21:18:04 +0530

    Add files via upload

--- lines.c
+++ lines.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
 
 int main() {
-    char ch;  
-    int lineCount = 0, tabCount = 0, spaceCount = 0;
+    char ch, new_string[100];
+    int spaces = 0, lines = 0, tab=0,j=0;
 
-    printf("Enter text :\n");
+    printf("Enter text\n");
 
-    // Read character by character from standard input
-    while ((ch = getchar()) != EOF) {
-        if (ch == '\n') {
-            lineCount++;  
-        } else if (ch == '\t') {
-            tabCount++;  
-        } else if (ch == ' ') {
-            spaceCount++; 
+    // Read characters one by one until #
+    while ((ch = getchar()) != '#') {
+        // Count spaces
+        if (ch == ' ') {
+            ch=';';
+			spaces++;
         }
+        
+        // Count newlines
+        if (ch == '\n') {
+            ch='"';
+			lines++;
+        }
+        if (ch== '\t'){
+        	ch='#';
+			tab++;
+        	
+		}
+		new_string[j++]=ch;
     }
-
-    // Display the results
-    printf("Number of lines: %d\n", lineCount);
-    printf("Number of tabs: %d\n", tabCount);
-    printf("Number of spaces: %d\n", spaceCount);
-
+    new_string[j]='\0';
+    // Print the results
+    printf("\nNumber of blank spaces: %d\n", spaces);
+    printf("Number of lines: %d\n", lines);
+	printf("Number of tabs: %d\n", tab);
+    printf("Altered string:\n");
+    puts(new_string);
     return 0;
 }
-
